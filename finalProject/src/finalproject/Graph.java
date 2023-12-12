b1d89892a23ee2b3ca5e741eac555534998918e6
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2023-12-12 00:23:24 -0500

    Dec 12, 12:20 AM - Graph.java
    did getNeighbors correctly ??

--- finalProject/src/finalproject/Graph.java
+++ finalProject/src/finalproject/Graph.java
@@ -9,18 +9,18 @@ public class Graph {
 
     private ArrayList<Tile> verticesArrayG;
 
-    private ArrayList<Edge> edgesArrayG;
+    private ArrayList<Edge> allEdges;
 
 
     // TODO level 2: initialize and assign all variables inside the constructor
-	public Graph(ArrayList<Tile> vertices) { // IS THIS EVEN WELL DONE?? like i'm not exactly building a graph
+	public Graph(ArrayList<Tile> vertices) {
 
         //  a constructor that builds the graph given a list containing all of its vertices.
         //  This graph should NOT contain any edges. The constructor should be used to initialize
         //  the vertices of the graph and whichever fields you decided to use in this class.
 
 		this.verticesArrayG = vertices;
-        this.edgesArrayG = new ArrayList<>();
+        this.allEdges = new ArrayList<>();
 	}
 
     public ArrayList<Tile> getVertices(){
@@ -30,21 +30,33 @@ public class Graph {
     // TODO level 2: add an edge to the graph
     public void addEdge(Tile origin, Tile destination, double weight){
     	Edge newEdge = new Edge(origin, destination, weight);
-        edgesArrayG.add(newEdge);
+        allEdges.add(newEdge);
     }
     
 //     TODO level 2: return a list of all edges in the graph
 	public ArrayList<Edge> getAllEdges() {
-		return this.edgesArrayG;
+		return this.allEdges;
 	}
   
 	// TODO level 2: return list of tiles adjacent to t
-	public ArrayList<Tile> getNeighbors(Tile t) { // THIS IS WRONG FIX IT
+	public ArrayList<Tile> getNeighbors(Tile t) { // is this ok???
 
         // a method that takes a Tile as input and returns an ArrayList containing all the Tiles adjacent
         // to it in this graph.
 
-    	ArrayList<Tile> neighborsOfTile = t.neighbors;
+    	ArrayList<Tile> neighborsOfTile = new ArrayList<>();
+
+        for (int i=0 ; i<this.allEdges.size() ; i++){
+
+            Edge curEdge = this.allEdges.get(i);
+
+            if (curEdge.origin.equals(t)){
+                Tile tDestination = curEdge.destination;
+                neighborsOfTile.add(tDestination);
+            }
+
+        }
+
         return neighborsOfTile;
     }
 	
@@ -101,17 +113,17 @@ public class Graph {
 		return totalCost;
 	}
 
-    public double computePathCostDistance(ArrayList<Tile> path) {
-
-        double totalCost = 0.0;
-        ArrayList<Edge> edgesArray = edgesFromTilesDistance(path);
-
-        for (int i=0 ; i<edgesArray.size() ; i++){
-            totalCost = totalCost + edgesArray.get(i).weight;
-        }
-
-        return totalCost;
-    }
+//    public double computePathCostDistance(ArrayList<Tile> path) {
+//
+//        double totalCost = 0.0;
+//        ArrayList<Edge> edgesArray = edgesFromTilesDistance(path);
+//
+//        for (int i=0 ; i<edgesArray.size() ; i++){
+//            totalCost = totalCost + edgesArray.get(i).weight;
+//        }
+//
+//        return totalCost;
+//    }
 
    
     public static class Edge{
