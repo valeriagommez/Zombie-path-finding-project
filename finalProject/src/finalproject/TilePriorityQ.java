877e281733db85f2fc80ac5e66418419d197f1d7
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2023-12-13 12:55:25 -0500

    Dec 13, 1:00 PM - TilePriorityQ.java
    fixed NullPointerError

--- finalProject/src/finalproject/TilePriorityQ.java
+++ finalProject/src/finalproject/TilePriorityQ.java
@@ -2,13 +2,12 @@ package finalproject;
 
 import java.util.ArrayList;
 
-
 import finalproject.system.Tile;
 
 public class TilePriorityQ {
 	// TODO level 3: Add fields that can help you implement this data type
 
-	private ArrayList<Tile> priorityQueue = new ArrayList<>();
+	private ArrayList<Tile> priorityQueue;
 
 	private int size;
 
@@ -16,18 +15,14 @@ public class TilePriorityQ {
 		return this.priorityQueue;
 	}
 
-	private void swap(int index1, int index2, ArrayList<Tile> array){
+	private void swap(int index1, int index2, ArrayList<Tile> array) {
 		Tile temp = array.get(index2);
-
 		array.set(index2, array.get(index1));
 		array.set(index1, temp);
-
-		return;
 	}
 
-	private void addUpHeap(Tile tileToAdd){
-
-		this.priorityQueue.add(0, null);
+	private void addUpHeap(Tile tileToAdd, ArrayList<Tile> priorityQueue) {
+//		System.out.println(priorityQueue); // debugging
 
 		this.size = size + 1;
 		priorityQueue.add(size, tileToAdd);
@@ -35,18 +30,24 @@ public class TilePriorityQ {
 
 		while ( (i<1) && (priorityQueue.get(i).costEstimate < priorityQueue.get(i/2).costEstimate) ) {
 
-			swap(i, i/2, this.priorityQueue); // swapping object at i and i/2
+			swap(i, (i/2), priorityQueue); // swapping object at i and i/2
 
 			i = i/2;
 		}
 	}
 
-	private void buildHeap(ArrayList<Tile> vertices){
+	private ArrayList<Tile> buildHeap(ArrayList<Tile> vertices) {
+
+		ArrayList<Tile> builtHeap = new ArrayList<>();
+
+		builtHeap.add(0, null);
 
 		for (int i=0 ; i<vertices.size() ; i++){
-			addUpHeap(vertices.get(i)); // do this or the buildHeapFast() method in the slides
+			addUpHeap(vertices.get(i), builtHeap); // do this or the buildHeapFast() method in the slides
 			// (30 - Heaps, slide 85)
 		}
+
+		return builtHeap;
 	}
 
 	public TilePriorityQ (ArrayList<Tile> vertices) {
@@ -54,9 +55,8 @@ public class TilePriorityQ {
 		// a constructor that builds a priority queue with the Tiles received as input.
 
 		this.priorityQueue = new ArrayList<>();
-		this.size = 0;
-
-		buildHeap(vertices);
+//		this.size = 0;
+		this.priorityQueue = buildHeap(vertices);
 	}
 	
 	// TODO level 3: implement remove min as seen in class
@@ -65,13 +65,17 @@ public class TilePriorityQ {
 
 		int i = startIndex;
 
-		while( 2*i <= maxIndex ){
+		while ( 2*i <= maxIndex ){
 
 			int child = 2*i;
 
 			if (child < maxIndex) {
 
-				System.out.println(priorityQueue.get(2)); // this prints null when it shouldn't
+//				System.out.println(this.size); // size is 9, so why is everything null?
+//				System.out.println(this.priorityQueue.size()); // here size is 10 (good bc index 0 is null)
+//				System.out.println(priorityQueue.size());
+//				System.out.println(this.priorityQueue);
+//				System.out.println(priorityQueue);
 
 				if (priorityQueue.get(child + 1).costEstimate < priorityQueue.get(child).costEstimate){  // this line is causing the error 
 						child = child + 1;
@@ -94,15 +98,20 @@ public class TilePriorityQ {
 
 	public Tile removeMin() {
 
-		// a method that takes no inputs and removed the Tile with highest priority (i.e. minimun estimate cost)
+		// a method that takes no inputs and removed the Tile with highest priority (i.e. minimum estimate cost)
 		// from the queue.
+
+		if (priorityQueue.size() == 1) {
+			return null;
+		}
+
+//		System.out.println(this.priorityQueue);
 
 		Tile removedTile = this.priorityQueue.get(1);
 
-		swap(1, size, this.priorityQueue); // swapping first and last
-//		this.priorityQueue.add(1, this.priorityQueue.get(size));
-//		this.priorityQueue.remove(size);
+		swap(1, this.size, this.priorityQueue); // swapping first and last
 
+		this.priorityQueue.remove(size);
 		this.size = size - 1;
 
 		downHeap(1, size, this.priorityQueue);
@@ -113,16 +122,11 @@ public class TilePriorityQ {
 	// TODO level 3: implement updateKeys as described in the pdf
 	public void updateKeys(Tile t, Tile newPred, double newEstimate) {
 
-		// a method that takes as input a Tile t. If such tile belongs to the queue, the method updates which Tile is
-		// predicted to be the predecessor of t in the minimum weight path that leads from a source tile to t as well
-		// as the estimated cost for this path. Note that this information should be stored in the appropriate fields
-		// from the Tile class, and after these updates, the queue should remain a valid min heap.
-
 		boolean tileBelongs = false;
 		ArrayList<Tile> queueBefore = this.priorityQueue;
 		Tile tileToModify = null;
 
-		for (int i=0 ; i<queueBefore.size() ; i++) {
+		for (int i=1 ; i<queueBefore.size() ; i++) {
 
 			if (queueBefore.get(i).equals(t)){
 				tileToModify = queueBefore.get(i);
@@ -144,8 +148,17 @@ public class TilePriorityQ {
 				}
 			}
 
-//			this.priorityQueue = buildHeap(newVertices);
-			buildHeap(newVertices);
+			TilePriorityQ updatedQueue = new TilePriorityQ(newVertices);
+
+			this.priorityQueue = updatedQueue.priorityQueue;
+
+//			If such tile belongs to the queue, the method updates which Tile is predicted
+//			to be the predecessor of t in the minimum weight path that leads from a source tile to t
+//			as well as the estimated cost for this path. Note that this information should be stored
+//			in the appropriate fields from the Tile class, and after these updates, the queue should
+//			remain a valid min heap
+
+
 		}
 
 		return;
