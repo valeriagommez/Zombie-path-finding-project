d7299e3395f39660811c54d1a28573ba14adba31
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2023-12-15 13:06:59 -0500

    Dec 15, 1:05 PM - Graph.java

--- finalProject/src/finalproject/Graph.java
+++ finalProject/src/finalproject/Graph.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import finalproject.system.Tile;
 
 public class Graph {
-	// TODO level 2: Add fields that can help you implement this data type
+
+    // TODO level 2: Add fields that can help you implement this data type
 
     private ArrayList<Tile> verticesArrayG;
 
@@ -13,39 +14,38 @@ public class Graph {
 
 
     // TODO level 2: initialize and assign all variables inside the constructor
-	public Graph(ArrayList<Tile> vertices) {
+    public Graph(ArrayList<Tile> vertices) {
 
         //  a constructor that builds the graph given a list containing all of its vertices.
         //  This graph should NOT contain any edges. The constructor should be used to initialize
         //  the vertices of the graph and whichever fields you decided to use in this class.
 
-		this.verticesArrayG = vertices;
+        this.verticesArrayG = vertices;
         this.allEdges = new ArrayList<>();
-	}
+    }
 
-    public ArrayList<Tile> getVertices(){
+    public ArrayList<Tile> getVertices() {
         return this.verticesArrayG;
     }
-	
+
     // TODO level 2: add an edge to the graph
     public void addEdge(Tile origin, Tile destination, double weight){
-    	Edge newEdge = new Edge(origin, destination, weight);
+        Edge newEdge = new Edge(origin, destination, weight);
         allEdges.add(newEdge);
     }
-    
-//     TODO level 2: return a list of all edges in the graph
-	public ArrayList<Edge> getAllEdges() {
-//        System.out.println(allEdges);
-		return this.allEdges;
-	}
-  
-	// TODO level 2: return list of tiles adjacent to t
-	public ArrayList<Tile> getNeighbors(Tile t) { // is this ok???
+
+    //     TODO level 2: return a list of all edges in the graph
+    public ArrayList<Edge> getAllEdges() {
+        return this.allEdges;
+    }
+
+    // TODO level 2: return list of tiles adjacent to t
+    public ArrayList<Tile> getNeighbors(Tile t) { // is this ok???
 
         // a method that takes a Tile as input and returns an ArrayList containing all the Tiles adjacent
         // to it in this graph.
 
-    	ArrayList<Tile> neighborsOfTile = new ArrayList<>();
+        ArrayList<Tile> neighborsOfTile = new ArrayList<>();
 
         for (int i=0 ; i<this.allEdges.size() ; i++){
 
@@ -61,7 +61,7 @@ public class Graph {
         return neighborsOfTile;
     }
 
-    private Edge getEdge(Tile origin, Tile destination){
+    public Edge getEdge(Tile origin, Tile destination){
 
         for (int i=0 ; i<this.allEdges.size() ; i++){
 
@@ -75,7 +75,7 @@ public class Graph {
         return null;
     }
 
-	public double computePathCost(ArrayList<Tile> path) {
+    public double computePathCost(ArrayList<Tile> path) {
 
         double totalCost = 0.0;
 
@@ -89,21 +89,21 @@ public class Graph {
 
         }
 
-		return totalCost;
-	}
-   
+        return totalCost;
+    }
+
     public static class Edge{
-    	Tile origin;
-    	Tile destination;
-    	double weight;
+        Tile origin;
+        Tile destination;
+        double weight;
 
         // TODO level 2: initialize appropriate fields
         public Edge(Tile s, Tile d, double cost){
-        	this.origin = s;
+            this.origin = s;
             this.destination = d;
             this.weight = cost;
         }
-        
+
         // TODO level 2: getter function 1
         public Tile getStart(){
             return this.origin;
@@ -113,7 +113,7 @@ public class Graph {
         public Tile getEnd() {
             return this.destination;
         }
-        
+
     }
-    
+
 }
